34f539f9c4ee9eae8f6ff1be21fdf935e723337a
Sainath Adapa <adapasainath@gmail.com>
2017-01-26 22:07:09 +0530

    added the prediction part to server

--- plugin/simple_client.py
+++ plugin/simple_client.py
@@ -1,6 +1,6 @@
 import httplib
 
 c = httplib.HTTPConnection('localhost', 8080)
-c.request('POST', '/process', '{}')
+c.request('POST', '/process', '{"input": ["how", "about"]}')
 doc = c.getresponse().read()
 print doc
--- plugin/simple_server.py
+++ plugin/simple_server.py
@@ -1,12 +1,21 @@
+from __future__ import division
+from __future__ import print_function
+from builtins import input
+import os
+import pickle
+from prediction_engine.predict import predict
 import json
-
 from bottle import run, post, request, response
+
+pkl_file = 'prediction_engine/nGramData.pkl'
+with open(pkl_file, 'rb') as handle:
+    dict_words = pickle.load(handle)
 
 @post('/process')
 def my_process():
-  # req_obj = json.loads(request.body.read())
-  # do something with req_obj
-  # ...
-  return 'All done'
+  req_obj = request.body.read().decode('UTF-8')
+  input_data = json.loads(req_obj)['input']
+  predicted = predict(dict_words, input_data)
+  return predicted
 
 run(host='localhost', port=8080, debug=True)
